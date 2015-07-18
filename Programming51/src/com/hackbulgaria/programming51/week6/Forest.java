f23a47c64aad2f51ca87766adb1d44e1a4b0895c
momchil-lukanov <momchil.lukanov@gmail.com>
2015-07-18 18:13:13 +0300

    gps

---
+++ Programming51/src/com/hackbulgaria/programming51/week6/Forest.java
@@ -0,0 +1,8 @@
+package com.hackbulgaria.programming51.week6;
+
+public class Forest implements Tile {
+	private String att = "!!";
+	public String render(){
+		return att;
+	}
+}
---
+++ Programming51/src/com/hackbulgaria/programming51/week6/Mountain.java
@@ -0,0 +1,8 @@
+package com.hackbulgaria.programming51.week6;
+
+public class Mountain implements Tile {
+	private String att = "/\\";
+	public String render(){
+		return att;
+	}
+}
