3298406fea87678b75a6cbc1d64eedd04b207ab9
momchil-lukanov <momchil.lukanov@gmail.com>
2015-07-24 19:07:31 +0300

    24.07

---
+++ Programming51/src/com/hackbulgaria/programming51/week6/Pair.java
@@ -0,0 +1,15 @@
+package com.hackbulgaria.programming51.week6;
+
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) { 
+    this.first = first;
+    this.second = second;
+    }
+
+  public T getFirst() { return first; }
+
+  public T getSecond() { return second; }
+}
